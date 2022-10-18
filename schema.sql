de2dbca758f5a7990c7b782fba2c86284a7968d9
sarah17moore <you@example.com>
2022-10-18 12:42:29 -0700

    Upload updated file

--- schema.sql
+++ schema.sql
@@ -34,8 +34,10 @@ CREATE TABLE salaries (
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+	from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  	PRIMARY KEY (emp_no, title, from_date)
 );
 CREATE TABLE dept_employee (
 	emp_no INT NOT NULL,
@@ -48,3 +50,8 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 );
 
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM dept_employee;
+SELECT * FROM salaries;
+SELECT * FROM titles;
